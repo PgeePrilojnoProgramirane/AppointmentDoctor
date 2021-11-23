aa32b314391ef69e02a10d7eb34b28cf2783d0e2
PgeePrilojnoProgramirane <katia_semkova@yahoo.com>
2021-11-23 12:11:05 +0200

    Add project files.

---
+++ AppointmentDoctor/Controllers/AccountController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using AppointmentDoctor.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AppointmentDoctor.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public AccountController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Login()
+        {
+            return View();
+        }
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+    }
+}
